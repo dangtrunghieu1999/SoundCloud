fd547ba14b4565d535d48135a4d4efbc7d7b0e26
Dang Trung Hieu <hieudt230899@gmail.com>
2020-12-05 10:25:25 +0700

    commit code

---
+++ SoundCloud/SoundCloud/Features/Home/ViewCell/RecentlyCellCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  RecentlyCellCollectionViewCell.swift
+//  SoundCloud
+//
+//  Created by Dang Trung Hieu on 12/3/20.
+//  Copyright © 2020 Dang Trung Hieu. All rights reserved.
+//
+
+import UIKit
+
+class SongPlayerCollectionViewCell: BaseCollectionViewCell {
+    
+    // MARK: - UI Elements
+    
+    
+    // MARK: - View LifeCycles
+    
+    override func initialize() {
+        super.initialize()
+    }
+    
+}
